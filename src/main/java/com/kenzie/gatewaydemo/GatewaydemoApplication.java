4cf346ddf3ea65791384a33cf64695e04ca359af
zohrashaik-kenzie <zohra.shaik@kenzie.academy>
2022-04-01 00:01:16 -0500

    first commit

---
+++ src/main/java/com/kenzie/gatewaydemo/GatewaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.kenzie.gatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenzie/gatewaydemo/GatewaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenzie.gatewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
